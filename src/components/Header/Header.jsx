a8411f87e2867d1a084365de045b78b9eb17a0d0
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-04-22 23:52:21 -0300

    UPDATE: link fopr the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ import styles from './Header.module.css';
 const Navbar = () => {
   return (
     <nav className={styles.header}>
-      <Link to="/ProjectEasySpeak" className={styles.anchor}>Home</Link>
+      <Link to="/https://ricardobertolucci.github.io" className={styles.anchor}>Home</Link>
       <Link to="/easyspeak" className={styles.anchor}>EasySpeak</Link>
       <Link to="/blog" className={styles.anchor}>Blog</Link>
       <Link to="/query" className={styles.anchor}>Agende sua consulta</Link>
